6037f275e0b91b295597cdecddcd0beb836a47ac
Bulosan10 <bulosanjohann@gmail.com>
2025-08-06 18:20:26 -0700

    Main

---
+++ src/banking/bankingApp.java
@@ -0,0 +1,28 @@
+package banking;
+
+import java.util.Scanner;
+
+
+public class bankingApp {
+    int accountNo = 20013, pin = 456789;
+    String name;
+    float initializeBalance = 0;
+    
+    public boolean validateAccount(int accn, int pi){
+        
+         return accn == accountNo && pi == pin;
+    }
+    
+    public int listAccount(){
+        return accountNo;
+    }
+    
+    
+    
+    
+    
+    public void viewBalance(){
+    
+    
+    }
+}
