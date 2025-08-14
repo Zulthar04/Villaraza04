ffbcec3fcef179bc26e6255587f88f9bb6618523
Bulosan10 <bulosanjohann@gmail.com>
2025-08-13 17:08:28 -0700

    New panel

--- src/Main/MAIN.java
+++ src/Main/MAIN.java
@@ -17,40 +17,75 @@ public class MAIN {
         System.out.println("2.Doctors Appointment");
         System.out.println("3.Shopping");
         
+        bankingApp bApp[] = new bankingApp[10];
+        int size = 0;
+        
         System.out.print("Enter Choice: ");
         int choice = sr.nextInt();
         
         switch(choice){
             case 1:
-                bankingApp bs = new bankingApp();
-                
-               int attempts = 3;
-               
-                while(attempts > 0){
-                    
-                    System.out.print("Enter Account No: ");
-                    int accountNo = sr.nextInt();
-                    
-                    System.out.println("Enter Pin: ");
-                    int pin = sr.nextInt();
-                    
-                    if(bs.validateAccount(accountNo, pin)){
-                        System.out.println("LOGIN SUCCESSFULLY");
+                int resp;
+                do {
+                System.out.println("1.Register Account");
+                System.out.println("2.Login Account");
+                System.out.println("3.View All Accounts");
+                System.out.println("Enter Selection");
+                int action = sr.nextInt();
+                switch(action){
+                    case 1:
+                        bApp[size] = new bankingApp();
                         
+                        System.out.print("Enter Account No: ");
+                        bApp[size].setAccountNo(sr.nextInt());
+                        
+                        System.out.print("Enter Pin: ");
+                        bApp[size].setPin(sr.nextInt());
+                       
+                        System.out.println("Account Registered Successfully");
+                         size++;
                         break;
+                    case 2:
+                       int attempts = 3;
+                       boolean found = false;
+                       
+                       while(attempts > 0 && !found){
+                           System.out.println("Enter Account No: ");
+                           int acc = sr.nextInt();
+                           
+                           System.out.println("Enter Pin: ");
+                           int pn = sr.nextInt();
+                           
+                           for(int i = 0; i < size; i++){
+                               if(bApp[i].verifyAccount(acc, pn)){
+                                   System.out.println("LOGIN SUCCESSFUL");
+                                   found = true;
+                                   break;
+                               }
+                                 }
+                               if(!found){
+                                 attempts--;
+                                 if(attempts < 0){
+                                     System.out.println("ATTEMPTS LIMIT REACHED!");
+                                     System.out.println("Access Denied.");
+                                 }else{
+                                     System.out.println("Login failed");
+                                     System.out.println("Attempts left: "+attempts);
+                                     System.exit(0);
+                               
+                               }
+                           }
+                       }
+                        break;
+                    default:
                         
-                    }else{
-                        attempts--;
-                        System.out.println("LOGIN FAILED!");
-                        System.out.println("Remaning Attempts "+attempts);
-                        
-                        if(attempts ==0){
-                            System.out.println("Max Attempts reached.");
-                            System.out.println("Access Denied");
-                        }
-                    }
-                        
+                    case 3:
+                        break;
+                   
                 }
+                    System.out.println("Do you want to continue? (1 = Yes/No = 0)");
+                     resp = sr.nextInt();
+                }while(resp == 1);
                 break;
             case 2:
                 break;
@@ -61,7 +96,4 @@ public class MAIN {
                 System.out.println("Try again.");
         }
     }
-    
-    
 }
-
--- src/banking/bankingApp.java
+++ src/banking/bankingApp.java
@@ -4,25 +4,48 @@ import java.util.Scanner;
 
 
 public class bankingApp {
-    int accountNo = 20013, pin = 456789;
-    String name;
-    float initializeBalance = 0;
-    
-    public boolean validateAccount(int accn, int pi){
-        
-         return accn == accountNo && pi == pin;
+    private int accountNo;
+    private float initialBalance;
+    private int pin;
+
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public int getAccountNo() {
+        return accountNo;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public int getPin() {
+        return pin;
     }
     
-    public int listAccount(){
+   
+    
+    
+    
+    
+    
+    
+    
+    public boolean verifyAccount(int acc, int pn){
+        
+            if(acc == accountNo && pn == pin){
+                return true;
+            }else{
+                return false;
+            }
+    }
+    
+    public int setAccount(){
         return accountNo;
     }
     
-    
-    
-    
-    
     public void viewBalance(){
     
-    
-    }
+        }
 }
